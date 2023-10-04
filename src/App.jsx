a8bffb71471edb6002559b383bc1302b524fae0b
Kristjan Lääne <xonproject@gmail.com>
2023-10-04 21:50:27 +0300

    rr setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Contact } from './pages/Contact'
 
 function App() {
-  return <>Hi</>
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+        </nav>
+        <Route path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export const Home = () => {
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+        nobis? Quos atque sint voluptatibus sit natus aspernatur repudiandae
+        tempore assumenda harum laudantium dolore at, magni sed placeat tenetur
+        itaque iusto.{' '}
+      </p>
+    </div>
+  )
+}
